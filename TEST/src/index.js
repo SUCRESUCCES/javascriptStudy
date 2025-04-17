1168e97efb9f6c7349c13f92b8c7b725153c7abd
밍 <sucresucces@icloud.com>
2025-04-17 17:03:26 +0900

    250417 변수와 상수

--- TEST/src/index.js
+++ TEST/src/index.js
@@ -1,6 +1,33 @@
-let name = "수민";
+// 변수는 변화하는 값을 저장하는 것
+
+// let name = "수민";
 name = "sumin";
 console.log(name);
 
+// 변수 명명 규칙
+// 1.변수명에는 $기호와_기호를 제외한 다른 기호는 사용할 수 없다
+// 2.변수명의 맨 앞에는 숫자를 사용할 수 없다
+// 3. 변수명에는 예약어를 사용할 수 없다
+
+// 변수명에는 $기호와_기호를 제외한 다른 기호는 사용할 수 없다
 let $name;
-let name^;
+// let name^;
+
+// 변수명의 맨 앞에는 숫자를 사용할 수 없다
+// let 1st
+let user1;
+let test123ing;
+
+// 변수명에는 예약어를 사용할 수 없다
+// let var;
+// let if;
+
+// 카멜표기법
+let userInfo;
+let isTrueOrFalse;
+
+// 상수 : 변하지 않는 값을 저장하는 저장소
+const birthday = "12-21";
+const name = "sumin";
+// 대문자로 상수 작성
+const EMAIL_ADDRESS = "123@gmail.com";
